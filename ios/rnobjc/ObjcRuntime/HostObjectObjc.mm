f191bd529b30a1e4b3d21a71ec92f5c2a7e5a64b
shirakaba <14055146+shirakaba@users.noreply.github.com>
2022-08-07 11:01:17 +0900

    toString and NSStringTransformLatinToHiragana

--- ios/rnobjc/ObjcRuntime/HostObjectObjc.mm
+++ ios/rnobjc/ObjcRuntime/HostObjectObjc.mm
@@ -1,18 +1,25 @@
 #import "HostObjectObjc.h"
 #import <jsi/jsi.h>
+#import <Foundation/Foundation.h>
 
 // Returns the value for any given property accessed.
-jsi::Value HostObjectObjc::get(jsi::Runtime& runtime, const jsi::PropNameID& propName) {
-  // 1️⃣ Grab the prop name
-  auto name = propName.utf8(runtime);
+jsi::Value HostObjectObjc::get(jsi::Runtime& rt, const jsi::PropNameID& propName) {
+  auto name = propName.utf8(rt);
 
   if (name == "toString") {
-    // 2️⃣ Initialise a HostFunction
-    auto toString = [] (jsi::Runtime& runtime, const jsi::Value&, const jsi::Value*, size_t) -> jsi::Value {
-      return jsi::String::createFromAscii(runtime, "[object HostObjectObjc]");
+    auto toString = [] (
+      jsi::Runtime& rt, const jsi::Value&, const jsi::Value*, size_t
+    ) -> jsi::Value {
+      return jsi::String::createFromAscii(rt, "[object HostObjectObjc]");
     };
-    // 3️⃣ Return a JSI Function based on that HostFunction
-    return jsi::Function::createFromHostFunction(runtime, jsi::PropNameID::forAscii(runtime, "toString"), 0, toString);
+
+    return jsi::Function::createFromHostFunction(
+      rt, jsi::PropNameID::forAscii(rt, "toString"), 0, toString
+    );
+  }
+  
+  if (name == "NSStringTransformLatinToHiragana"){
+    return jsi::String::createFromUtf8(rt, NSStringTransformLatinToHiragana.UTF8String);
   }
   
   return jsi::Value::undefined();
@@ -21,7 +28,6 @@ jsi::Value HostObjectObjc::get(jsi::Runtime& runtime, const jsi::PropNameID& pro
 // Returns the list of keys.
 std::vector<jsi::PropNameID> HostObjectObjc::getPropertyNames(jsi::Runtime& rt) {
   std::vector<jsi::PropNameID> result;
-  // 4️⃣ Add "toString" to the list of keys
-//  result.push_back(jsi::PropNameID::forAscii(rt, "toString"));
+  result.push_back(jsi::PropNameID::forAscii(rt, "NSStringTransformLatinToHiragana"));
   return result;
 }
