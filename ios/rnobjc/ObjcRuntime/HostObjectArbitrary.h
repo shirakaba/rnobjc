a35f9a0840f9a5c590d61c0b8030e69818ceeb94
shirakaba <14055146+shirakaba@users.noreply.github.com>
2022-08-08 22:26:39 +0900

    More sketching

--- ios/rnobjc/ObjcRuntime/HostObjectArbitrary.h
+++ ios/rnobjc/ObjcRuntime/HostObjectArbitrary.h
@@ -2,12 +2,19 @@
 
 using namespace facebook;
 
+enum HostObjectArbitraryType {
+  CLASS_INSTANCE,
+//  SERIALISABLE,
+  OTHER,
+};
+
 class JSI_EXPORT HostObjectArbitrary: public jsi::HostObject {
 
 public:
   HostObjectArbitrary(void *nativeRef);
-  ~HostObjectArbitrary();
   void *m_nativeRef;
+  HostObjectArbitraryType m_type;
   jsi::Value get(jsi::Runtime& rt, const jsi::PropNameID& name) override;
+  void set(jsi::Runtime& runtime, const jsi::PropNameID& propName, const jsi::Value& value) override;
   std::vector<jsi::PropNameID> getPropertyNames(jsi::Runtime& rt) override;
 };
--- ios/rnobjc/ObjcRuntime/HostObjectArbitrary.mm
+++ ios/rnobjc/ObjcRuntime/HostObjectArbitrary.mm
@@ -4,28 +4,61 @@
 
 // The constructor
 HostObjectArbitrary::HostObjectArbitrary(void *nativeRef)
-: m_nativeRef(nativeRef) {}
-
-// The destructor
-HostObjectArbitrary::~HostObjectArbitrary() {}
+: m_nativeRef(nativeRef) {
+  @try {
+    if([(__bridge NSObject *)m_nativeRef isKindOfClass:[NSObject class]]){
+      m_type = HostObjectArbitraryType::CLASS_INSTANCE;
+      return;
+    }
+  }
+  @catch (NSException *exception) {
+    // Handles both ObjC and C++ exceptions as long as it's 64-bit.
+    // https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/Exceptions/Articles/Exceptions64Bit.html
+  }
+  
+  m_type = HostObjectArbitraryType::OTHER;
+}
 
 // Returns the value for any given property accessed.
 jsi::Value HostObjectArbitrary::get(jsi::Runtime& rt, const jsi::PropNameID& propName) {
   auto name = propName.utf8(rt);
-
+  
   if (name == "toString") {
-    auto toString = [] (
-      jsi::Runtime& rt, const jsi::Value&, const jsi::Value*, size_t
-    ) -> jsi::Value {
-      return jsi::String::createFromAscii(rt, "[object HostObjectObjc]");
-    };
-
     return jsi::Function::createFromHostFunction(
-      rt, jsi::PropNameID::forAscii(rt, "toString"), 0, toString
+      rt,
+      jsi::PropNameID::forAscii(rt, "toString"),
+      0,
+      [] (jsi::Runtime& rt, const jsi::Value&, const jsi::Value*, size_t) -> jsi::Value {
+        return jsi::String::createFromAscii(rt, "[object HostObjectArbitrary]");
+      }
     );
   }
   
+  if(name == "Symbol.toStringTag"){
+    // Handles: console.log('arbitrary.NSString:', arbitrary.NSString);
+    return jsi::String::createFromAscii(rt, "[object HostObjectClassInstance]");
+  }
+  
+  // Runtime type encodings:
+  // https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/ObjCRuntimeGuide/Articles/ocrtTypeEncodings.html#//apple_ref/doc/uid/TP40008048-CH100
+  
+  if(m_type != HostObjectArbitraryType::CLASS_INSTANCE){
+    // TODO: consider how to support serialisable HostObjects.
+    // Seems like we should allow indexing into enums and structs, but do we
+    // also do all serialisable (NSDictionary, NSArray, NSString, std::string)?
+    // Do we get that for free with NSObject's runtime getter anyway?
+    // Do we auto-marshal totally serialisable objects? I can see it falling
+    // apart for NSDictionary<string, any>, large objects, and
+    return jsi::Value::undefined();
+  }
+  
+  
+  
   return jsi::Value::undefined();
+}
+
+void HostObjectArbitrary::set(jsi::Runtime& runtime, const jsi::PropNameID& propName, const jsi::Value& value) {
+//  auto name = propName.utf8(runtime);
 }
 
 // Returns the list of keys.
