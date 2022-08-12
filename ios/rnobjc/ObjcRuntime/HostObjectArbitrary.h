f0790d35c69bb02d4bb343b60ebd79e091ad1eec
shirakaba <14055146+shirakaba@users.noreply.github.com>
2022-08-12 10:58:59 +0900

    Delete comments

--- ios/rnobjc/ObjcRuntime/HostObjectArbitrary.h
+++ ios/rnobjc/ObjcRuntime/HostObjectArbitrary.h
@@ -6,7 +6,6 @@ enum HostObjectArbitraryType {
   OTHER,
   CLASS,
   CLASS_INSTANCE,
-//  SERIALISABLE,
 };
 
 class JSI_EXPORT HostObjectArbitrary: public jsi::HostObject {
@@ -20,9 +19,5 @@ public:
   void set(jsi::Runtime& runtime, const jsi::PropNameID& propName, const jsi::Value& value) override;
   std::vector<jsi::PropNameID> getPropertyNames(jsi::Runtime& rt) override;
   
-  // Returns a jsi::Function that, when called, will:
-  // - invoke a class method with the given selector on the given class;
-  // - capture its return value;
-  // - marshals it to a JSI Object (either a jsi::Value or a jsi::HostObject as appropriate).
   jsi::Function invokeMethod(jsi::Runtime &runtime, std::string methodName, SEL sel);
 };
